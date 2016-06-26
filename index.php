57d072789ef7c73592840fb162b0bdd36b11f832
Electra Chong <electrachong@gmail.com>
2016-06-26 16:32:13 -0700

    Add code to test if php is rendered properly

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<h1> test </h1>
+<html>
+    <head>
+        <title><!-- INSERT TITLE HERE --></title>
+    </head>
+    
+    <body>
+        <h1>A simple php webapp</h1>
+        <?php echo '<p>If php is enabled, you will see this text display</p>'; ?>
+    </body>
+</html>
+
