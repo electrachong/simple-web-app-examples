5568c7454fdb7769ec7acc13a8bfdebb264a07b4
Electra Chong <electrachong@gmail.com>
2016-06-26 17:31:47 -0700

    Add a form and a page to handle it

---
+++ action.php
@@ -0,0 +1,25 @@
+<?php
+  // The global $_POST variable allows you to access the data sent with the POST method
+  // To access the data sent with the GET method, you can use $_GET
+  // The htmlspecialchars() function makes sure special HTML characters such as '<' and '>' are encoded so people cannot inject HTML or Javascript tags into the page through your form.
+  $name = htmlspecialchars($_POST['name']);
+  $picture  = htmlspecialchars($_POST['picture']);
+?>
+
+Here you go, <?php echo $name; ?>! </br>
+
+<!-- You can use control structures with php -->
+<?php
+if (strcmp($picture == "pink_feathers") {
+    echo '<img src="https://pbs.twimg.com/media/Cl6LQ5GUgAAnhI0.jpg:large" />'
+} 
+elseif ($picture == "rainbow_lei") {
+    echo '<img src="https://pbs.twimg.com/media/Cl6LQ5EUoAEossh.jpg:large" />'
+}
+elseif ($picture == "batman") {
+    echo '<img src="https://pbs.twimg.com/media/Cl43GDJUgAAPYYV.jpg:large" />'
+}
+else {
+    echo '<img src="https://pbs.twimg.com/media/Cl1MA5SUoAA56CX.jpg:large" />'
+} 
+?>
--- index.php
+++ index.php
@@ -6,9 +6,31 @@
     <body>
         <h1>A simple php webapp</h1>
         
-        #If php is enabled, you will see this text display
+        <!-- If php is enabled (must be installed on server), you will see this text display -->
         <?php echo '<p>Use php to show this text!</p>'; ?>
-        <?php phpinfo(); ?>
+        
+        <h2>A basic form</h2>
+        
+        <!-- When this HTML form is submitted, it will send the form data via a http POST request to the web server at the designated path (action.php).
+            The http request will contain this message body:
+                name=[formvalue]&picture=[formvalue]
+            The code in action.php is then called to handle the response!    
+        -->
+
+        <form action="action.php" method="post">
+            <p>Your name: <input type="text" name="name" /></p>
+            <p>What would you like?</p>
+            <input type="radio" name="picture" value="pink_feathers" checked> Pink Feathers<br>
+                <input type="radio" name="picture" value="rainbow_lei"> Rainbow Lei<br>
+                <input type="radio" name="picture" value="batman"> Batman<br>
+                <input type="radio" name="picture" value="gym"> Gym<br>
+            <p><input type="submit" /></p>
+        </form>
+        
+        <!-- Note: In this case we are using the POST method because we are sending some data variables to the server, and want an appropriate response. You could use the GET method if you simply want to request a certain resource. 
+        FMI: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms/Sending_and_retrieving_form_data 
+        -->
+        
     </body>
 </html>
 
